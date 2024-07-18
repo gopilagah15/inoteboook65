88aec106a2d4eca1ab9e6e127097befeeae4c254
unknown <you@example.com>
2024-07-18 08:42:56 +0530

    new

--- src/component/Noteitem.js
+++ src/component/Noteitem.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/noteContext'
 
 const Noteitem = ({note}) => {
-
+    const context = useContext(noteContext);
+    const {deleteNote} = context;
   return (
     <div>
           <div className="col-md-4">
@@ -9,7 +11,7 @@ const Noteitem = ({note}) => {
     <div className="card-body"> 
     <div className=" d-flex align-items-center mx-2">
    <h5 className="card-title">{note.title}</h5>
-   <i class="fa-solid fa-trash mx-2"></i>
+   <i class="fa-solid fa-trash mx-2" onClick={()=>{return deleteNote(note._id)}}></i>
    <i class="fa-solid fa-pen-to-square"></i>   </div> 
    <p className="card-text">{note.description}</p> 
 
